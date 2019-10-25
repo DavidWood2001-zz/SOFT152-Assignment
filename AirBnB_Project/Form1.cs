e59e28e088e69ec4ff96aa969a3ff5a08ccfdfe6
David <davidwood1301@gmail.com>
2019-10-25 21:43:13 +0100

    Added comments to Form1.cs so it is easier to tell what is happenning

--- AirBnB_Project/Form1.cs
+++ AirBnB_Project/Form1.cs
@@ -25,24 +25,28 @@ namespace AirBnB_Project
 
         private void EditButton_Click(object sender, EventArgs e)
         {
+            //when the edit button is pressed the edit form pops up
             editForm edit = new editForm();
             edit.Show();
         }
 
         private void AddButton_Click(object sender, EventArgs e)
         {
+            //when the add button is pressed the add form pops up
             addForm add = new addForm();
             add.Show();
         }
 
         private void SearchButton_Click(object sender, EventArgs e)
         {
+            //when the search button is pressed the search form pops up
             searchForm search = new searchForm();
             search.Show();
         }
 
         private void DeleteButton_Click(object sender, EventArgs e)
         {
+            //when the delete button is pressed the search form pops up
             deleteForm delete = new deleteForm();
             delete.Show();
         }
