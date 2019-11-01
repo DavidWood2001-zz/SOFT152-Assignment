8d2d452a4490d47574eeb7efeed4628330e4abc1
David Wood <davidwood1301@gmail.com>
2019-11-01 18:31:02 +0000

    Created the Neighbourhood class with all attributes

--- AirBnB_Project/Neighbourhood.cs
+++ AirBnB_Project/Neighbourhood.cs
@@ -8,5 +8,39 @@ namespace AirBnB_Project
 {
     class Neighbourhood
     {
+        string name;
+        int numProperties;
+        Property[] arrayProperties;
+
+        //CONSTRUCTOR
+        public Neighbourhood(string inName, Property[] inArrayProperties)
+        {
+            name = inName;
+            numProperties = inArrayProperties.Length;
+            arrayProperties = inArrayProperties;
+        }
+
+        //GETTERS
+        public string getName()
+        {
+            return name;
+        }
+        public int getNumProperties()
+        {
+            return numProperties;
+        }
+        public Property[] getArrayProperties()
+        {
+            return arrayProperties;
+        }
+        //SETTERS
+        public void setName(string inName)
+        {
+            name = inName;
+        }
+        public void setArrayProperties(Property[] inArrayProperties)
+        {
+            arrayProperties = inArrayProperties;
+        }
     }
 }
